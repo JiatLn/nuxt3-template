2ff5232518a2d03da55fa5e688ae8cc557b92a6e
林佳涛 <lne128gene980@163.com>
2022-05-07 14:06:37 +0800

    deps: add unocss config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,13 +6,6 @@ export default defineNuxtConfig({
     '@unocss/nuxt',
   ],
   unocss: {
-    // presets
-    uno: true, // enabled `@unocss/preset-uno`
-    icons: true, // enabled `@unocss/preset-icons`
-    attributify: true, // enabled `@unocss/preset-attributify`,
-
-    // core options
-    shortcuts: [],
-    rules: [],
+    preflight: true,
   },
 })
