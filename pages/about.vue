41b1902c0dc93a4d1667cc815827807116a8ac87
林佳涛 <lne128gene980@163.com>
2022-05-20 17:33:00 +0800

    update: about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>关于我们</div>
+  <div text-center flex="c col" h-40vh>
+    <h4 mb-10px>这是一个开源的nuxt3项目模板.</h4>
+    <div>
+      项目地址：
+      <a href="https://github.com/JiatLn/nuxt3-template" target="_blank" text-brand-primary>
+        Nuxt3-Template
+      </a>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+</style>
