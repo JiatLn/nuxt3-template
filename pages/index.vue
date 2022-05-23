336c2b6b9a6ac5ebc2ace194ab6fd80869960500
林佳涛 <lne128gene980@163.com>
2022-05-23 14:27:14 +0800

    deps: add unocss preset-icons

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,9 @@ const data = await fn.sayHi('haha')
     <div>这里是首页</div>
     <p>data: {{ data }}</p>
     <n-button>naive-ui</n-button>
+    <div text="xl red-400" i-carbon-home />
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+</style>
