d3df2005ef3f09c210fdf0a6ff8e1b32aacd62c7
林佳涛 <lne128gene980@163.com>
2022-07-28 13:58:57 +0800

    update: unocss config

--- unocss.config.ts
+++ unocss.config.ts
@@ -10,14 +10,14 @@ import {
 
 export default defineConfig({
   shortcuts: [
-    ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+    ['btn', 'px-4 py-1 rounded inline-block bg-#ffa631 text-white cursor-pointer hover:bg-#ffa631dd disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-#ffa631'],
     ['flex-c', 'flex items-center justify-center'],
   ],
   theme: {
     colors: {
       brand: {
-        primary: '#FFA631', // used like bg-brand-primary
+        primary: '#ffa631', // used like bg-brand-primary
         light: '#80eec0', // used like bg-brand-light
       },
     },
