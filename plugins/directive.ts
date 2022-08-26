9f167e8e28998d735d201d871571385f8dd6f6c7
林佳涛 <lne128gene980@163.com>
2022-08-26 09:53:23 +0800

    feat: add v-focus directive

---
+++ plugins/directive.ts
@@ -0,0 +1,9 @@
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.directive('focus', {
+    mounted(el: HTMLInputElement) {
+      setTimeout(() => {
+        el.focus()
+      }, 1000)
+    },
+  })
+})
