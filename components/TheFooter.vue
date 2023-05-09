4e1b714b8d6a40f58ea390b3589b6e698790bcaf
林佳涛 <lne128gene980@163.com>
2023-05-09 11:24:55 +0800

    update: ui

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <footer w-full text-center text-brand-primary flex justify="center" items-center h-40px>
+  <footer w-full text-brand-primary flex items-center px-4 h-40px border-t text="14px">
     Copyright &copy;
     <a href="https://github.com/JiatLn/nuxt3-template" target="_blank">
       JiatLn 2022
@@ -10,5 +10,4 @@
 </template>
 
 <style scoped lang="scss">
-
 </style>
--- unocss.config.ts
+++ unocss.config.ts
@@ -10,15 +10,15 @@ import {
 
 export default defineConfig({
   shortcuts: [
-    ['btn', 'px-4 py-1 rounded inline-block bg-#ffa631 text-white cursor-pointer hover:bg-#ffa631dd disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-#ffa631'],
+    ['btn', 'px-4 py-1 rounded-full inline-block bg-#f40 text-white cursor-pointer hover:bg-#f40a disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-#f40'],
     ['flex-c', 'flex items-center justify-center'],
     ['cp', 'cursor-pointer'],
   ],
   theme: {
     colors: {
       brand: {
-        primary: '#ffa631', // used like bg-brand-primary
+        primary: '#f40', // used like bg-brand-primary
         light: '#80eec0', // used like bg-brand-light
       },
     },
